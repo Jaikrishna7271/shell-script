697bd6fdc5b028be3d6a8f30167d9dd3dabfb3ff
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-20 08:56:20 +0530

    variables-28

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+#IMP QUESTIONS FOR INTERVIEW
 echo "all variables: $@"
 echo "no of variables passed: $#"
+echo "script name: $0"
+echo "prsent working directory: $PWD"
+echo "home directory of current user: $HOME" 
