260550f014a4a7b1b0958447c0e7833d54a87046
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-20 08:24:09 +0530

    variables-25

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo"please enter username:"
+echo "please enter username:"
 read USERNAME #HERE USERNAME is variable
-echo"please enter passwd"
+echo "please enter passwd:"
 read PASSWD
 echo "username is:$USERNAME, passwd is:$PASSWD"
