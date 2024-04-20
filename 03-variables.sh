1f3e563587eb6b3b31b267a22129ef271370dcdf
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-20 08:09:18 +0530

    variables-23

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=sachin
+PERSON2=sehwag
+
+echo "$PERSON1: hello suresh,h r u?"
+echo "$PERSON2: hi ramesh,i am fine. h r u?"
+echo "$PERSON1: i am fine too, how is ur work?"
+echo "$PERSON2: not bad"
