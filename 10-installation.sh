f1a1b48a638b411e389acce90c6669795a27b3e9
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-20 10:07:46 +0530

    variables-30

--- 10-installation.sh
+++ 10-installation.sh
@@ -10,5 +10,22 @@ else
 fi
 
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+ echo "installation of mysql...failure"
+ exit 1
+ else
+  echo "installation of mysql is success"
+fi
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+ echo "installation of git...failure"
+ exit 1
+ else
+  echo "installation of git is success"
+fi
+
 
 echo "is script proceeding?"
