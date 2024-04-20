cccdae1c3f6a2461b4f26abaf2541abfb35343ec
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-20 08:03:20 +0530

    variables-21

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-this is also sample scriptS
+#!/bin/bash
+
+echo "ramesh: hello suresh,h r u?"
+echo "suresh: hi ramesh,i am fine. h r u?"
+echo "ramesh: i am fine too, how is ur work?"
+echo "suresh: not bad"
