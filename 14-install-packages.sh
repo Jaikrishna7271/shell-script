93134c9f473b176d6ca5bdf3130982b368c33f28
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-21 09:12:10 +0530

    variables-33

---
+++ 14-install-packages.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+R="\e[31m"
+G="\e[33m"
+N="\e[0m"
+
+echo "script startedexectuting at: $TIMESTAMP"
+
+VALIDATE(){
+  if [ $1 -ne 0 ]
+  then
+       echo -e "$2...$R failure $N"
+       exit 1
+    else
+       echo -e "$2...$G success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+ echo "please run this script with root access"
+ exit 1
+else
+ echo "you are a super user"
+fi
+
+for i in $@
+do
+ echo "package to install: $i"
+ dnf installed $i &>>$LOGFILE
+ if [ $? -eq 0 ]
+ then
+   echo "$i already installed...SKIPPING"
+ else
+   echo "$i not install...need to install"
+ fi   
+done
