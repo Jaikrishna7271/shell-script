e4790a75c0c0ed1f7fb652df2ea02de19f3789ba
Jaikrishna7271 <jayakrishna7271@gmail.com>
2024-04-25 08:06:33 +0530

    newchangs1.sh file

---
+++ 15-currentscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+COURSE="DEVOPS FROM CURRENT USER SCRIPT"
+
+echo "before calling other script, course: $COURSE"
+echo "process id of current script: $$"
+
+#./16-otherscript.sh
+
+echo "after calling other script, course: $COURSE"
